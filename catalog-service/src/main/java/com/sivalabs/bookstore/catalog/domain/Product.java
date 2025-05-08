30ba887e85b4a311261a48ffd200d28db7d9d457
Mustafa Ahmed El Henawy <melhensdssdawy@c.com>
2025-05-08 16:22:07 +0300

    Initial commit for feature/new-feature

---
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/domain/Product.java
@@ -0,0 +1,7 @@
+package com.sivalabs.bookstore.catalog.domain;
+
+import java.math.BigDecimal;
+
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price) {}
+
+
